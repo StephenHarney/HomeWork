1c399743b8098a01111d89941ef6a69cf4f8363c
StephenHarney <g00367573@atu.ie>
2023-10-04 14:06:07 +0100

    adding updated CalculatorController.java

--- src/main/java/ie/atu/hello/CalculatorController.java
+++ src/main/java/ie/atu/hello/CalculatorController.java
@@ -29,61 +29,65 @@ public class CalculatorController {
                 break;
 
             case "divide":
+                if (num2 == 0) {
+                    throw new IllegalArgumentException("Divide is not equal to 0");
+                }
+                result = num1 / num2;
+
+                break;
+            default:
+
+                throw new IllegalArgumentException("ERROR Operation nto working");
+        }
+        Calculate object = new Calculate(result, operation);
+        return object;
+    }
+}
+
+   /* @GetMapping("/calculate/{num1}/{num2}/{operation}")
+    public Calculate Calculate(@PathVariable int num1, @PathVariable int num2, @PathVariable String operation){
+
+        int result;
+        switch (operation){
+
+            case"add":
+
+                result = num1 + num2;
+                //links http://localhost:8080/calculate/6/2/add
+                break;
+
+            case "subtract":
+                result = num1 - num2;
+                //links http://localhost:8080/calculate/10/5/subtract
+                break;
+
+            case "multiply":
+                result = num1 * num2;
+
+                //links http://localhost:8080/calculate/4/3/multiply
+                    break;
+            case "divide":
                 if (num2 == 0){
                     throw new IllegalArgumentException("Divide is not equal to 0");
                 }
                 result = num1/num2;
-
-                break;
+               //links http://localhost:8080/calculate/8/2/divide
+                    break;
             default:
+                throw  new IllegalArgumentException("ERROR Operation nto working");
 
-               throw  new IllegalArgumentException("ERROR Operation nto working");
+
         }
-        Calculate object = new Calculate(result,operation);
-        return object;
+        Calculate Object = new Calculate(result,operation);
+        return Object;
+
+
     }
 
-   /* @GetMapping
-    public String Calculate(@PathVariable int num1, @PathVariable int num2, @PathVariable String operation, @PathVariable int result){
-
-        switch (operation){
-            case "ADD":
-            case"add":
-                result = num1 +num2;
-                break;
-            case "SUBTRACt":
-            case "subtract":
-                result = num1 -num2;
-                break;
-            case "MULTIPLY":
-            case "multiply":
-                result = num1 * num2;
-            case "DIVIDE":
-            case "Divide":
-                result = num1/num2;
-
-            default:
 
 
 
-
-
-        }
-        return "operation = " + result;
-
-    }*/
-
-    /*@GetMapping("/calculate{num1}")
-    public int add(@PathVariable int num1){
-        return num1;
-
-    }*/
-
-}
-   /* @GetMapping("/calculate")
-    public String calculate(@RequestParam String num1, @RequestParam String num2, @RequestParam String operation, @RequestParam int subtract){
-            return "num1 = num2 = operation = -  " + num1 + num2 + operation + subtract;
-    }*/
+}*/
 
 
 
